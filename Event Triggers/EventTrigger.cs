3b7b999afc41179fd3f74f1bbf3384db81725aef
Michal <malpa.w.klapkach@gmail.com>
2023-06-09 03:33:33 +0200

    Add separated component implementation of event triggers

---
+++ Event Triggers/EventTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Bipolar.EventTriggers
+{
+    public abstract class EventTrigger<T> : MonoBehaviour where T : BaseEventData
+    {
+        public event System.Action<T> OnEventTriggered;
+
+        [SerializeField]
+        private UnityEvent<T> onEventTrigger;
+
+        protected void Execute(T data)
+        {
+            onEventTrigger.Invoke(data);
+            OnEventTriggered?.Invoke(data);
+        }
+    }
+
+    public abstract class EventTrigger : EventTrigger<BaseEventData>
+    { }
+
+    public abstract class PointerEventTrigger : EventTrigger<PointerEventData>
+    { }
+
+    public abstract class AxisEventTrigger : EventTrigger<AxisEventData> 
+    { }
+}

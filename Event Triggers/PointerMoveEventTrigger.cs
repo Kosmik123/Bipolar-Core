b2c5e322ff6cba9599c49c104dbbd39398a05b93
Michal <malpa.w.klapkach@gmail.com>
2023-06-22 23:17:01 +0200

    Add unity version check on PointerEventTrigger

--- Event Triggers/PointerMoveEventTrigger.cs
+++ Event Triggers/PointerMoveEventTrigger.cs
@@ -2,6 +2,7 @@
 
 namespace Bipolar.EventTriggers
 {
+#if UNITY_2021_1_OR_NEWER
     public class PointerMoveEventTrigger : PointerEventTrigger, IPointerMoveHandler
     {
         public void OnPointerMove(PointerEventData eventData)
@@ -9,4 +10,5 @@ namespace Bipolar.EventTriggers
             Execute(eventData);
         }
     }
+#endif
 }

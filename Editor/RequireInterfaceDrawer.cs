a35b81d31fd5377798c664462bb6fb3aa69d6f44
Kosmik <malpa.w.klapkach@gmail.com>
2023-09-12 15:12:48 +0200

    Change not working SerializedInterface to working RequireInterface attribute
    It's only half of a success but at least it's something. We don't have
    to explicitly add validation code to OnValidate anymore

---
+++ Editor/RequireInterfaceDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(RequireInterfaceAttribute))]
+public class RequireInterfaceDrawer : PropertyDrawer
+{
+    private const string errorMessage = "Property is not a reference type";
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType == SerializedPropertyType.ObjectReference)
+        {
+            var requiredAttribute = attribute as RequireInterfaceAttribute;
+            EditorGUI.BeginProperty(position, label, property);
+            property.objectReferenceValue = EditorGUI.ObjectField(position, label, property.objectReferenceValue, requiredAttribute.RequiredType, true);
+            EditorGUI.EndProperty();
+        }
+        else
+        {
+            var previousColor = GUI.color;
+            GUI.color = Color.red;
+            EditorGUI.LabelField(position, label, new GUIContent(errorMessage));
+            GUI.color = previousColor;
+        }
+    }
+}

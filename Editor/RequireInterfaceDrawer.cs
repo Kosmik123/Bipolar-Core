7e0f8682aadc0ac7621e200ad43f16c3baf95484
Kosmik <malpa.w.klapkach@gmail.com>
2023-09-24 19:45:50 +0200

    Fix RequireInterface related issues

--- Editor/RequireInterfaceDrawer.cs
+++ Editor/RequireInterfaceDrawer.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using Bipolar.Core;
 
-[CustomPropertyDrawer(typeof(RequireInterfaceAttribute))]
-public class RequireInterfaceDrawer : PropertyDrawer
+namespace Bipolar.Editor
 {
-    private const string errorMessage = "Property is not a reference type";
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(RequireInterfaceAttribute))]
+    public class RequireInterfaceDrawer : PropertyDrawer
     {
-        if (property.propertyType == SerializedPropertyType.ObjectReference)
+        private const string errorMessage = "Property is not a reference type";
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            var requiredAttribute = attribute as RequireInterfaceAttribute;
-            EditorGUI.BeginProperty(position, label, property);
-            property.objectReferenceValue = EditorGUI.ObjectField(position, label, property.objectReferenceValue, requiredAttribute.RequiredType, true);
-            EditorGUI.EndProperty();
-        }
-        else
-        {
-            var previousColor = GUI.color;
-            GUI.color = Color.red;
-            EditorGUI.LabelField(position, label, new GUIContent(errorMessage));
-            GUI.color = previousColor;
+            if (property.propertyType == SerializedPropertyType.ObjectReference)
+            {
+                var requiredAttribute = attribute as RequireInterfaceAttribute;
+                EditorGUI.BeginProperty(position, label, property);
+                property.objectReferenceValue = EditorGUI.ObjectField(position, label, property.objectReferenceValue, requiredAttribute.RequiredType, true);
+                EditorGUI.EndProperty();
+            }
+            else
+            {
+                var previousColor = GUI.color;
+                GUI.color = Color.red;
+                EditorGUI.LabelField(position, label, new GUIContent(errorMessage));
+                GUI.color = previousColor;
+            }
         }
     }
 }

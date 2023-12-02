ae2496e092a26e932becb17466deff3e8e321246
Michal <malpa.w.klapkach@gmail.com>
2023-12-02 08:34:36 +0100

    Create Action Input Provider implementation

---
+++ Input/InputManagerClickActionProvider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using UInput = UnityEngine.Input;
+
+namespace Bipolar.Input
+{
+    public class InputManagerClickActionProvider : MonoBehaviour, IActionInputProvider
+    {
+        public event System.Action OnAction;
+
+        [SerializeField]
+        private MouseButton button;
+
+        private bool isPressed;
+
+        private void Update()
+        {
+            int button = (int)this.button;
+            if (UInput.GetMouseButtonDown(button))
+            {
+                isPressed = true;
+            }
+            else if (UInput.GetMouseButtonUp(button))
+            {
+                if (isPressed)
+                    OnAction?.Invoke();
+                isPressed = false;
+            }
+        }
+    }
+}

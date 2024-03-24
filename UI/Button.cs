f1991c136f181acb44aa60c93205ead90dd8c7a1
Michal <malpa.w.klapkach@gmail.com>
2024-03-24 12:29:20 +0100

    Add text label to button

--- UI/Button.cs
+++ UI/Button.cs
@@ -1,3 +1,5 @@
+using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Bipolar.UI
@@ -5,6 +7,19 @@ namespace Bipolar.UI
     public class Button : UnityEngine.UI.Button
     {
         public event System.Action OnClicked;
+        
+        [SerializeField]
+        private TMP_Text label;
+
+        public string Text
+        {
+            get => label ? label.text : string.Empty;
+            set
+            {
+                if (label)
+                    label.text = value;
+            }
+        }
 
         public override void OnPointerClick(PointerEventData eventData)
         {

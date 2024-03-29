075a6b9e098362a84b8d0d5e8315cb2581bac16d
Michal <malpa.w.klapkach@gmail.com>
2024-03-29 22:34:43 +0100

    Create a SerializedGuid type and custom editor for it. However I can see how much Unity doesn't want me to do it

---
+++ Editor/SerializedGuidDrawer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Bipolar.Editor
+{
+    [CustomPropertyDrawer(typeof(SerializedGuid))]
+    public class SerializedGuidDrawer : PropertyDrawer
+    {
+        private const float buttonWidth = 65;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var a = property.FindPropertyRelative("a");
+            var b = property.FindPropertyRelative("b");
+            var c = property.FindPropertyRelative("c");
+            var d = property.FindPropertyRelative("d");
+            var e = property.FindPropertyRelative("e");
+            var f = property.FindPropertyRelative("f");
+            var g = property.FindPropertyRelative("g");
+            var h = property.FindPropertyRelative("h");
+            var i = property.FindPropertyRelative("i");
+            var j = property.FindPropertyRelative("j");
+            var k = property.FindPropertyRelative("k");
+            var backingGuid = new Guid(a.intValue,
+                (short)b.intValue,
+                (short)c.intValue,
+                (byte)d.intValue, 
+                (byte)e.intValue, 
+                (byte)f.intValue, 
+                (byte)g.intValue, 
+                (byte)h.intValue, 
+                (byte)i.intValue, 
+                (byte)j.intValue, 
+                (byte)k.intValue);
+
+            var buttonRect = position;
+            bool isSmall = position.width - EditorGUIUtility.labelWidth < 2 * buttonWidth;
+            buttonRect.width = isSmall ? 0 : buttonWidth;
+
+            var textRect = position;
+            textRect.width -= buttonRect.width;
+            buttonRect.x += textRect.width + 1;
+
+            var labelRect = textRect;
+            labelRect.width = EditorGUIUtility.labelWidth;
+            
+            textRect.x += labelRect.width + 2;
+            textRect.width -= labelRect.width + 2;
+
+            bool isPrefabOverride = property.prefabOverride;
+            EditorGUI.LabelField(labelRect, label, isPrefabOverride ? EditorStyles.boldLabel : EditorStyles.label);
+            EditorGUI.TextField(textRect, backingGuid.ToString());
+
+            if (GUI.Button(buttonRect, "New"))
+            {
+                property.serializedObject.Update();
+                var newGuid = Guid.NewGuid();
+                var bytes = newGuid.ToByteArray();
+                a.intValue = BitConverter.ToInt32(bytes, 0);
+                b.intValue = BitConverter.ToInt16(bytes, 4);
+                c.intValue = BitConverter.ToInt16(bytes, 6);
+                d.intValue = bytes[8];
+                e.intValue = bytes[9];
+                f.intValue = bytes[10];
+                g.intValue = bytes[11];
+                h.intValue = bytes[12];
+                i.intValue = bytes[13];
+                j.intValue = bytes[14];
+                k.intValue = bytes[15];
+                property.serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
---
+++ SerializedGuid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bipolar
+{
+    [Serializable]
+    public struct SerializedGuid : IComparable, IComparable<SerializedGuid>, IEquatable<SerializedGuid>, IFormattable
+    {
+        [SerializeField] private int a; 
+        [SerializeField] private short b; 
+        [SerializeField] private short c; 
+        [SerializeField] private byte d; 
+        [SerializeField] private byte e; 
+        [SerializeField] private byte f; 
+        [SerializeField] private byte g; 
+        [SerializeField] private byte h; 
+        [SerializeField] private byte i; 
+        [SerializeField] private byte j;
+        [SerializeField] private byte k;
+
+        public static implicit operator Guid(SerializedGuid serializedGuid) => serializedGuid.ToGuid();
+        public static explicit operator SerializedGuid(Guid guid) => new SerializedGuid(guid);
+        public static SerializedGuid New() => new SerializedGuid(Guid.NewGuid());
+        public int CompareTo(SerializedGuid other) => ToGuid().CompareTo(other.ToGuid());
+        public int CompareTo(object obj) => ToGuid().CompareTo(obj);
+        public readonly bool Equals(SerializedGuid other) => other.ToGuid().Equals(other.ToGuid());
+        public string ToString(string format, IFormatProvider formatProvider) => ToGuid().ToString(format, formatProvider);
+        public Guid ToGuid() => new Guid(a, b, c, d, e, f, g, h, i, j, k);
+    
+        private SerializedGuid(Guid input)
+        {
+            var bytes = input.ToByteArray();
+            a = BitConverter.ToInt32(bytes, 0);
+            b = BitConverter.ToInt16(bytes, 4);
+            c = BitConverter.ToInt16(bytes, 6);
+            d = bytes[8];
+            e = bytes[9];
+            f = bytes[10];
+            g = bytes[11];
+            h = bytes[12];
+            i = bytes[13];
+            j = bytes[14];
+            k = bytes[15];
+        }
+    }
+}

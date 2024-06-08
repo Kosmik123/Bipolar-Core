6d76ee22677bb130f8601528e433886808742aea
Kosmik <malpa.w.klapkach@gmail.com>
2024-06-08 14:52:12 +0200

    Hide Script reference in SelectionBase inspector

--- SelectionBase.cs
+++ SelectionBase.cs
@@ -6,4 +6,12 @@ namespace Bipolar
     public class SelectionBase : MonoBehaviour
     { }
 
+#if UNITY_EDITOR
+    [UnityEditor.CustomEditor(typeof(SelectionBase))]
+    public class SelectionBaseEditor : UnityEditor.Editor 
+    {
+        public override void OnInspectorGUI()
+        { }
+    }
+#endif
 }

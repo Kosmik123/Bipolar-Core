067eba811efeb7c703ed6da0b6bd453257a1d4c6
Kosmik <malpa.w.klapkach@gmail.com>
2024-06-08 09:00:14 +0200

    Finally working on UI part

---
+++ UI/UIButton.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Bipolar.UI
+{
+    public class UIButton : UnityEngine.UI.Button
+    {
+        public event System.Action OnClicked;
+        public event System.Action<bool> OnHighlightChanged;
+        
+        [SerializeField]
+        private TMP_Text label;
+        public TMP_Text Label => label;
+
+        public string Text
+        {
+            get => label ? label.text : string.Empty;
+            set
+            {
+                if (label)
+                    label.text = value;
+            }
+        }
+
+        protected override void Reset()
+        {
+           label = GetComponentInChildren<TMP_Text>();
+        }
+
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            base.OnPointerClick(eventData);
+            OnClicked?.Invoke();
+        }
+
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            base.OnPointerEnter(eventData);
+            OnHighlightChanged?.Invoke(true);
+        }
+
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            base.OnPointerExit(eventData);
+            OnHighlightChanged?.Invoke(false);
+        }
+    }
+
+}

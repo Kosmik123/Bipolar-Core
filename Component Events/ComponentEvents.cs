112a02478aeb05254ee438875761b2f16c7b6aea
Michal <malpa.w.klapkach@gmail.com>
2024-07-01 03:07:41 +0200

    Remove debug logs which were helping with IL2CPP compilations. For now IL2CPP is not supported

--- Component Events/ComponentEvents.cs
+++ Component Events/ComponentEvents.cs
@@ -35,7 +35,6 @@ namespace Bipolar.Prototyping.ComponentEvents
 
         private void Awake()
         {
-            Debug.Log("Let's check it this works");
             if (component == null)
             {
                 enabled = false;
@@ -45,65 +44,43 @@ namespace Bipolar.Prototyping.ComponentEvents
 
             var componentType = component.GetType();
             var events = componentType.GetEvents();
-            Debug.Log("We got type and events");
             int count = Mathf.Min(events.Length, eventsData.Length);
-
-            Debug.Log("Start iterating events");
             for (int i = 0; i < count; i++)
             {
-                Debug.Log($"Initialize event: {i}");
                 var unityEventData = eventsData[i];
                 var eventInfo = componentType.GetEvent(unityEventData.eventName);
                 unityEventData.EventInfo = eventInfo;
 
-                Debug.Log($"Event is: {eventInfo}");
                 ParameterExpression[] eventParameters = GetEventParameterExpressions(eventInfo);
 
                 Expression instanceExpression = Expression.Constant(unityEventData.UnityEvent);
                 var invokeUnityEventInfo = unityEventData.UnityEvent.GetType().GetMethod(nameof(UnityEvent.Invoke));
                 var unityEventParameters = invokeUnityEventInfo.GetParameters();
 
-                Debug.Log("We got parameters and events");
                 Expression body = null;
-                Debug.Log($"The event has {eventParameters.Length} parameters");
-
                 int possibleParametersCount = Mathf.Min(2, eventParameters.Length);
-                Debug.Log($"UnityEvent takes {unityEventParameters.Length} parameters");
-
                 if (unityEventParameters.Length > 0)
                 {
                     for (int a = 0; a < possibleParametersCount; a++)
                     {
-                        Debug.Log($"Lets check {a} parameter");
                         var argumentType = eventParameters[a].Type;
-                        Debug.Log($"Its type is {argumentType}");
                         if (eventDataTypesByArgumentType.ContainsKey(argumentType))
                         {
-                            Debug.Log($"UnityEvent found! We can create a body that passes a paremeter");
                             Expression passedParameter = eventParameters[a];
                             body = Expression.Call(instanceExpression, invokeUnityEventInfo, passedParameter);
                             break;
                         }
-                        Debug.Log($"But none UnityEvent takes {argumentType}");
                     }
                 }
 
-                Debug.Log("Lets check if body is null");
                 if (body == null)
                 {
-                    Debug.Log("It is!");
                     body = Expression.Call(instanceExpression, invokeUnityEventInfo);
                 }
-                Debug.Log($"Function body is {body}");
-                Debug.Log($"Event handler type is {eventInfo.EventHandlerType}");
-                Debug.Log($"Event type is {eventInfo.EventHandlerType}");
 
                 LambdaExpression lambda = Expression.Lambda(eventInfo.EventHandlerType, body, eventParameters);
 
-                Debug.Log($"We created lambda: {lambda}");
                 Delegate compiledDelegate = lambda.Compile();
-
-                Debug.Log($"Compiled Delegate is: {compiledDelegate}! Hooray!");
                 unityEventData.InvokeDelegate = compiledDelegate;
             }
         }
@@ -111,28 +88,18 @@ namespace Bipolar.Prototyping.ComponentEvents
         private static ParameterExpression[] GetEventParameterExpressions(EventInfo eventInfo)
         {
             Type eventHandlerType = eventInfo.EventHandlerType;
-            Debug.Log($"Type of Event Handler is {eventHandlerType}");
             MethodInfo invokeMethodInfo = eventHandlerType.GetMethod(nameof(Action.Invoke));
-            Debug.Log($"Method that is invoked is {invokeMethodInfo}");
             ParameterInfo[] parameterInfos = invokeMethodInfo.GetParameters();
-            Debug.Log($"Method linked to this event should have {parameterInfos.Length} parameters {parameterInfos}");
             ParameterExpression[] eventParameters = parameterInfos.Select(p => Expression.Parameter(p.ParameterType, p.Name)).ToArray();
-            Debug.Log($"Which gives us {eventParameters.Length} parameterExpressions: {eventParameters}");
-
             return eventParameters;
         }
 
         private void OnEnable()
         {
-            Debug.Log($"SUBSCRIBE");
             foreach (var eventDatum in eventsData)
             {
-                Debug.Log($"Iterate {eventDatum}");
                 EventInfo eventInfo = eventDatum.EventInfo;
-                Debug.Log($"It's event info is {eventInfo}");
-                Debug.Log($"We can add event handler to it");
                 eventInfo.AddEventHandler(component, eventDatum.InvokeDelegate);
-                Debug.Log($"Added");
             }
         }
 

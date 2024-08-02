490c3bb3488c361436bb816f155dfaf3eb27e843
Michal <malpa.w.klapkach@gmail.com>
2024-08-02 12:58:26 +0200

    UI system refactors and fixes

--- UI/Window.cs
+++ UI/Window.cs
@@ -1,10 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Bipolar.UI
 {
 	public class Window : MonoBehaviour
     {
+        public event System.Action<Window> OnClosed;
+
         [SerializeField]
         private UIButton closeButton;
+
+        private void OnEnable()
+        {
+            if (closeButton)
+                closeButton.OnClicked += Close; 
+        }
+
+        private void Close(UIButton button)
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            if (closeButton)
+                closeButton.OnClicked -= Close;
+            OnClosed?.Invoke(this);
+        }
     }
 }

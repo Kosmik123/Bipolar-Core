48349608aaecb1337351f0de4a175f6b96368595
Michal <malpa.w.klapkach@gmail.com>
2024-11-09 16:56:24 +0100

    Add more randomizers

---
+++ Randomizers/PositionRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Bipolar
+{
+    public class PositionRandomizer : GenericRandomizer<Transform>
+    {
+        [SerializeField]
+        private Vector3 minPosition = -Vector3.one;
+        [SerializeField]
+        private Vector3 maxPosition = Vector3.one;
+
+        [SerializeField]
+        private Space space;
+
+        [ContextMenu("Randomize")]
+        public override void Randomize()
+        {
+            var randomPosition = new Vector3(
+                Random.Range(minPosition.x, maxPosition.x),
+                Random.Range(minPosition.y, maxPosition.y),
+                Random.Range(minPosition.z, maxPosition.z));
+
+            if (space == Space.World)
+                RandomizedComponent.position = randomPosition;
+            else
+                RandomizedComponent.localPosition = randomPosition;
+        }
+    }
+}
---
+++ Randomizers/ScaleRandomizer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Bipolar
+{
+    public class ScaleRandomizer : GenericRandomizer<Transform>
+    {
+        [SerializeField]
+        private Vector3 minScale = Vector3.one;
+        [SerializeField]
+        private Vector3 maxScale = Vector3.one * 2;
+
+        [ContextMenu("Randomize")]
+        public override void Randomize()
+        {
+            RandomizedComponent.localScale = new Vector3(
+                Random.Range(minScale.x, maxScale.x),
+                Random.Range(minScale.y, maxScale.y),
+                Random.Range(minScale.z, maxScale.z));
+        }
+    }
+}
